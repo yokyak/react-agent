de890b6274ff414e5d0171b8a0410b486fad5df2
Henry <henryau01@gmail.com>
2018-01-16 14:36:02 -0800

    Optimized parseSQL

--- packages/react-agent-server/source/index.js
+++ packages/react-agent-server/source/index.js
@@ -5,7 +5,7 @@ module.exports = (server, database, queries, logger = false) => {
   const io = socketio(server);
   const Sequelize = require('sequelize');
   const chalk = require('chalk');
-  const Op = Sequelize.Op;
+  const { Op } = Sequelize;
 
   const sequelize = new Sequelize(database.name, database.user, database.password, {
     dialect: database.dialect,
@@ -17,47 +17,31 @@ module.exports = (server, database, queries, logger = false) => {
 
   const subscribedSockets = {};
 
-  io.on('connection', socket => {
-
-    socket.on('subscribe', ({ key }) => {
-      if(subscribedSockets[key]) {
-        if (!subscribedSockets[key].includes(socket)) {
-          subscribedSockets[key].push(socket);
+  const parseSQL = (queryStr, request) => {
+    const replacements = [];
+    let string = '';
+    let grabStr = false;
+    for (let i = 0; i < queryStr.length; i++) {
+      if (queryStr[i] === '$') {
+        let str = '';
+        grabStr = true;
+        while (grabStr) {
+          str += queryStr[i + 1];
+          i++;
+          if (queryStr[i + 1] === ',' || queryStr[i + 1] === ')' || queryStr[i + 1] === ';' || queryStr[i + 1] === ' ' || i + 1 === queryStr.length) {
+            grabStr = false;
+            i++;
+          }
         }
-      } else subscribedSockets[key] = [socket];
-    });
-
-    socket.on('emit', data => {
-      if (subscribedSockets[data.key]) {
-        runQuery(data.key, data.request, data.queryId, result => {
-          subscribedSockets[data.key].forEach(subSocket => {
-            subSocket.emit('subscriber', result);
-          });
-        });
+        if (request[str]) {
+          replacements.push(request[str]);
+          string += '?';
+        } else string += `$${str}`;
       }
-    });
-
-    socket.on('query', data => {
-      runQuery(data.key, data.request, data.queryId, result => {
-        console.log(chalk.bold('  Callback: '), 'success');
-        socket.emit('response', result);
-        console.log(chalk.bold('  Completed: '), data.key, data.queryId);
-      });
-    });
-
-    // Search through each key in subscribedSockets object and look for matching socket
-    // Remove matching socket from each of the arrays corresponding to the key
-    socket.on('disconnect', () => {
-      Object.keys(subscribedSockets).forEach(key => {
-        const i = subscribedSockets[key].indexOf(socket);
-        if (i > -1) {
-          const arr = subscribedSockets[key].slice();
-          arr.splice(i, 1);
-          subscribedSockets[key] = arr;
-        }
-      });
-    });
-  });
+      if (queryStr[i] !== undefined) string += queryStr[i];
+    }
+    return { string, replacements };
+  };
 
   const runQuery = (key, request, queryId, callback) => {
     if (logger) {
@@ -70,9 +54,8 @@ module.exports = (server, database, queries, logger = false) => {
         if (returned === false) {
           if (logger) console.log(chalk.bold.red(`  Pre-error: did not pass function #${i + 1}`));
           return callback({ preError: 'React Agent: Not all server pre functions passed.', queryId });
-        } else {
-          request = returned;
         }
+        request = returned;
       }
     }
 
@@ -81,14 +64,14 @@ module.exports = (server, database, queries, logger = false) => {
     if (typeof queries[key].query !== 'function') {
       const { string, replacements } = parseSQL(queries[key].query, request);
       sequelize.query(string, { replacements })
-        .then(response => {
+        .then((response) => {
           if (queries[key].callback) {
             callback({ key, response: queries[key].callback(response), queryId });
           } else {
             callback({ key, response, queryId });
           }
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(chalk.bold.red('  Error with database: '), chalk.yellow(error));
           if (queries[key].errorMessage) {
             callback({ databaseError: queries[key].errorMessage, queryId });
@@ -100,37 +83,51 @@ module.exports = (server, database, queries, logger = false) => {
       const promise = new Promise((resolve, reject) => {
         queries[key].query(resolve, reject, request);
       });
-      promise.then(response => {
+      promise.then((response) => {
         console.log(chalk.bold('  Query function: '), 'success');
         callback({ key, response, queryId })
       });
     }
   };
 
-  const parseSQL = (string, request) => {
-    const reg = new RegExp('[a-zA-Z0-9]+', 'g');
-    let foundVariable = false, start = 0, currentVariable = '';
-    const replacements = [];
-    for (let i = 0; i <= string.length; i++) {
-      let char = string[i];
-      if (foundVariable) {
-        if (i === string.length || !char.match(reg)) {
-          if (request[currentVariable]) {
-            replacements.push(request[currentVariable]);
-            string = string.substring(0, start) + '?' + string.substring(i, string.length);
-            i = i - (currentVariable.length - 1);
-          }
-          foundVariable = false;
-          currentVariable = '';
-        } else {
-          currentVariable += char;
+  io.on('connection', (socket) => {
+    socket.on('subscribe', ({ key }) => {
+      if (subscribedSockets[key]) {
+        if (!subscribedSockets[key].includes(socket)) {
+          subscribedSockets[key].push(socket);
         }
+      } else subscribedSockets[key] = [socket];
+    });
+
+    socket.on('emit', (data) => {
+      if (subscribedSockets[data.key]) {
+        runQuery(data.key, data.request, data.queryId, (result) => {
+          subscribedSockets[data.key].forEach((subSocket) => {
+            subSocket.emit('subscriber', result);
+          });
+        });
       }
-      if (char === '$') {
-        foundVariable = true;
-        start = i;
-      }
-    }
-    return { string, replacements };
-  };
-}
+    });
+
+    socket.on('query', (data) => {
+      runQuery(data.key, data.request, data.queryId, (result) => {
+        console.log(chalk.bold('  Callback: '), 'success');
+        socket.emit('response', result);
+        console.log(chalk.bold('  Completed: '), data.key, data.queryId);
+      });
+    });
+
+    // Search through each key in subscribedSockets object and look for matching socket
+    // Remove matching socket from each of the arrays corresponding to the key
+    socket.on('disconnect', () => {
+      Object.keys(subscribedSockets).forEach((key) => {
+        const i = subscribedSockets[key].indexOf(socket);
+        if (i > -1) {
+          const arr = subscribedSockets[key].slice();
+          arr.splice(i, 1);
+          subscribedSockets[key] = arr;
+        }
+      });
+    });
+  });
+};
