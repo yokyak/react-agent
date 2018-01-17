f5afd46a566a44190ec0c93f5b1a18d4a804d934
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2018-01-17 08:59:40 -0800

    changed server back to express

--- packages/react-agent-test-site/index.js
+++ packages/react-agent-test-site/index.js
@@ -1,19 +1,12 @@
 const path = require('path');
 const agent = require('./../react-agent-server');
 const actions = require('./actions');
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  let contentType = 'text/html', filePath = req.url;
-  if (filePath === '/') filePath = '/index.html';
-  else contentType = 'text/javascript';
-  fs.readFile('./build' + filePath, (err, data) => {
-    res.writeHead(200, { 'Content-Type': contentType });
-    res.write(data);
-    res.end();
-  });
-}).listen(3000);
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+const server = app.listen(3000);
 
 const database = {
   name: 'qxqigbwr',
