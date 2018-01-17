60d554b43a860d3560df1770e6bd95c873079a78
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2018-01-17 14:08:39 -0800

    added redux for client store

--- packages/react-agent-test-site/client/index.js
+++ packages/react-agent-test-site/client/index.js
@@ -24,7 +24,7 @@ const initialStore = {
 };
 
 render(
-  <Agent store={initialStore} logger="true">
+  <Agent store={initialStore} logger='false'>
     <App />
   </Agent>
   , document.querySelector('#root'));
