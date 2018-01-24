21018f1c9112843cb309592267cf982f22303dee
eric <ericcodesweb@gmail.com>
2018-01-24 15:02:29 -0800

    adds dotenv to test-site

--- packages/react-agent-test-site/index.js
+++ packages/react-agent-test-site/index.js
@@ -3,18 +3,19 @@ const agent = require('./../react-agent-server');
 const actions = require('./actions');
 const express = require('express');
 const app = express();
+require('dotenv').config()
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
 const server = app.listen(3000);
 
 const database = {
-  name: 'qxqigbwr',
-  user: 'qxqigbwr',
-  password: 'IU0b6NPNVmAwn6gVB6IK5W7mcXZ79IxX',
+  name: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   dialect: 'postgres',
-  host: 'baasu.db.elephantsql.com',
-  port: 5432
+  host: process.env.DB_URL,
+  port: process.env.DB_PORT
 };
 
 agent(server, actions, database, true);
